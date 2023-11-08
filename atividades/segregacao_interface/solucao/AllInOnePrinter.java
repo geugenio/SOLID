e767f941e06f8efd228b115082d91c3b26be05ca
Gabriel Eugênio <98301722+geugenio@users.noreply.github.com>
2023-11-07 23:37:20 -0300

    solução segregação de interface

---
+++ atividades/segregacao_interface/solucao/AllInOnePrinter.java
@@ -0,0 +1,16 @@
+public class AllInOnePrinter implements Impressora, Fax, Scanner {
+    @Override
+    public void print() {
+        // Implementação do método print
+    }
+
+    @Override
+    public void scan() {
+        // Implementação do método scan
+    }
+
+    @Override
+    public void fax() {
+        // Implementação do método fax
+    }
+}
---
+++ atividades/segregacao_interface/solucao/Impressora.java
@@ -0,0 +1,3 @@
+public interface Impressora{
+        void print();
+}
